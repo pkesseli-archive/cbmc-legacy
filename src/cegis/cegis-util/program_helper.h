1351a78bd42dc68aa6ec87d725ade44366ab65d0
pkesseli <pkesseli@dkr10.cs.ox.ac.uk>
2016-03-17 00:26:08 +0000

    Added missing helpers.

---
+++ src/cegis/cegis-util/program_helper.h
@@ -0,0 +1,35 @@
+/*******************************************************************
+
+ Module: Counterexample-Guided Inductive Synthesis
+
+ Author: Daniel Kroening, kroening@kroening.com
+         Pascal Kesseli, pascal.kesseil@cs.ox.ac.uk
+
+\*******************************************************************/
+
+#ifndef CEGIS_PROGRAM_HELPER_H_
+#define CEGIS_PROGRAM_HELPER_H_
+
+/**
+ * @brief
+ *
+ * @details
+ *
+ * @param gf
+ *
+ * @return
+ */
+class goto_programt &get_entry_body(class goto_functionst &gf);
+
+/**
+ * @brief
+ *
+ * @details
+ *
+ * @param gf
+ *
+ * @return
+ */
+const goto_programt &get_entry_body(const goto_functionst &gf);
+
+#endif /* CEGIS_PROGRAM_HELPER_H_ */
