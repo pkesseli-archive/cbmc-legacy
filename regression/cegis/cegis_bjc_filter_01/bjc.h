79f9a60fef71e81af220ca669585866cc95c7a74
pkesseli <pkesseli@dkr10.cs.ox.ac.uk>
2016-03-20 21:39:39 +0000

    First speed experiments.

--- regression/cegis/cegis_bjc_filter_01/bjc.h
+++ regression/cegis/cegis_bjc_filter_01/bjc.h
@@ -1,11 +1,38 @@
 #ifndef CEGIS_BJC_H_
 #define CEGIS_BJC_H_
 
-typedef struct abstract_heap abstract_heapt;
 typedef unsigned int word_t;
 typedef word_t ptr_t;
 typedef word_t data_t;
+typedef word_t index_t;
 
+#define MAX_LIST_SIZE 5
+#ifndef NPROG
+#define NPROG 4
+#endif
+
+typedef struct abstract_iterator
+{
+  _Bool is_valid;
+  ptr_t list;
+  index_t index;
+} abstract_iteratort;
+
+typedef struct abstract_list
+{
+  _Bool is_valid;
+  index_t size;
+  data_t content[MAX_LIST_SIZE];
+} abstract_listt;
+
+typedef struct abstract_heap
+{
+  abstract_listt lists[NPROG];
+  abstract_iteratort iterators[NPROG];
+} abstract_heapt;
+
+
+ptr_t __CPROVER_bjc_nondet_list(abstract_heapt * heap);
 ptr_t __CPROVER_bjc_new_list(abstract_heapt *heap);
 ptr_t __CPROVER_bjc_clone(abstract_heapt *heap, ptr_t list);
 ptr_t __CPROVER_bjc_iterator(abstract_heapt *heap, ptr_t list);
--- src/cegis/bjc/preprocessing/bjc_preprocessing.cpp
+++ src/cegis/bjc/preprocessing/bjc_preprocessing.cpp
@@ -9,6 +9,10 @@
 #include <cegis/bjc/preprocessing/bjc_query_factory.h>
 #include <cegis/bjc/preprocessing/bjc_preprocessing.h>
 
+// XXX: Debug
+#include <iostream>
+// XXX: Debug
+
 bjc_preprocessingt::bjc_preprocessingt(optionst &options,
     const symbol_tablet &st, const goto_functionst &gf) :
     options(options), original_program(st, gf), num_consts(0)
@@ -30,6 +34,17 @@ bool int_list_filter(const irep_idt &id, const typet &type)
   const irep_idt &type_id=type.id();
   return ID_signedbv == type_id || ID_unsignedbv == type_id;
 }
+
+std::string find_refactored_loop_id(goto_programt &body)
+{
+  body.compute_loop_numbers();
+  std::string loop_id("main.");
+  for (const goto_programt::instructiont instr : body.instructions)
+    if (instr.is_backwards_goto()
+        && instr.source_location.get_function() == "main") return loop_id+=
+        std::to_string(instr.loop_number);
+  assert(!"Refactored loop not found.");
+}
 }
 
 void bjc_preprocessingt::operator ()()
@@ -40,10 +55,12 @@ void bjc_preprocessingt::operator ()()
   goto_functionst &gf=original_program.gf;
   goto_inline(gf, ns, msg);
   goto_programt &body=get_entry_body(gf);
+
   default_cegis_constant_strategy(st, gf);
   original_program.ce_targets=find_nondet_instructions(body);
   var_ids.insert(std::make_pair(LAMBDA_OPERATOR_NAME, LAMBDA_OPERATOR_ID));
   num_consts=get_variable_op_ids(st, var_ids, &int_list_filter);
+  original_program.refactored_loop_id=find_refactored_loop_id(body);
 }
 
 void bjc_preprocessingt::operator ()(const size_t max_length)
@@ -62,7 +79,18 @@ void bjc_preprocessingt::operator ()(const size_t max_length)
   link_user_program_variable_ops(st, gf, instr, &int_list_filter,
       instrs.begin(), instrs.end());
   add_bjc_query(current_program, num_vars, num_consts, max_length);
-  options.set_option("unwind", static_cast<unsigned int>(max_length));
+  const size_t max_list_size=5u;
+  std::string unwind_set(current_program.refactored_loop_id);
+  unwind_set+=':';
+  unwind_set+=std::to_string(max_list_size);
+  unwind_set+=",__CPROVER_danger_execute.0";
+  unwind_set+=':';
+  unwind_set+=std::to_string(max_length);
+  // XXX: Debug
+  std::cout << "<unwindset>" << unwind_set << "</unwindset>" << std::endl;
+  options.set_option("unwinding-assertions", true);
+  // XXX: Debug
+  options.set_option("unwindset", unwind_set);
 }
 
 size_t bjc_preprocessingt::get_min_solution_size() const
