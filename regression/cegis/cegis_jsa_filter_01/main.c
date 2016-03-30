df55e9a110368c6cbd95e3c676df9e28887eb54b
Pascal Kesseli <pascal_kesseli@hotmail.com>
2016-03-30 09:19:32 +0100

    Updated JSA abstract transformers.

--- regression/cegis/cegis_jsa_filter_01/main.c
+++ regression/cegis/cegis_jsa_filter_01/main.c
@@ -1,4 +1,3 @@
-
 #ifndef __CPROVER
 #include "../../../src/ansi-c/library/jsa.c"
 #endif
@@ -6,4 +5,15 @@
 int main(void)
 {
   abstract_heapt * const heap=__CPROVER_jsa_create_heap();
+  const list_t list=__CPROVER_jsa_create_nondet_list(heap);
+  const unsigned int limit;
+  for (const iterator_t it=__CPROVER_jsa_iterator(heap, list);
+      __CPROVER_jsa_hasNext(heap, it);)
+  {
+    const data_t value=__CPROVER_jsa_next(heap, it);
+    if (data <= limit)
+    {
+      __CPROVER_jsa_remove(heap, it);
+    }
+  }
 }
