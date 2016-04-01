5776e2f85f42f9fff943b41318515495a9b60e6f
pkesseli <pkesseli@dkr13.cs.ox.ac.uk>
2016-04-01 20:13:56 +0100

    Intermediate commit for Windows tests.

--- src/cegis/jsa/facade/jsa_runner.cpp
+++ src/cegis/jsa/facade/jsa_runner.cpp
@@ -1,3 +1,9 @@
+#include <util/options.h>
+
+#include <cegis/facade/cegis.h>
+#include <cegis/options/parameters.h>
+#include <cegis/seed/null_seed.h>
+
 #include <cegis/jsa/preprocessing/jsa_preprocessing.h>
 #include <cegis/jsa/facade/jsa_runner.h>
 
@@ -10,9 +16,8 @@ int run_jsa(optionst &options, mstreamt &result, const symbol_tablet &st,
     const goto_functionst &gf)
 {
   jsa_preprocessingt preproc(options, st, gf);
-  // XXX: Debug
-  preproc();
-  preproc(1);
-  // XXX: Debug
+  const size_t max_sz=options.get_unsigned_int_option(CEGIS_MAX_SIZE);
+  const null_seedt seed;
+  //return run_cegis(learn, oracle, preproc, seed, max_sz, result);
   return 10;
 }
