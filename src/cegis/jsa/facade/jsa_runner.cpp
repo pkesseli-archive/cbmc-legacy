9b1c8b7ee093ed024179cbddddaa7e07a9c36f40
Pascal Kesseli <pkesseli@dkr10.cs.ox.ac.uk>
2016-04-12 17:19:28 +0100

    JSA extension

--- src/cegis/jsa/facade/jsa_runner.cpp
+++ src/cegis/jsa/facade/jsa_runner.cpp
@@ -18,6 +18,10 @@ int run_jsa(optionst &options, mstreamt &result, const symbol_tablet &st,
   jsa_preprocessingt preproc(options, st, gf);
   const size_t max_sz=options.get_unsigned_int_option(CEGIS_MAX_SIZE);
   const null_seedt seed;
+  // XXX: Debug
+  preproc();
+  preproc(1u);
+  // XXX: Debug
   //return run_cegis(learn, oracle, preproc, seed, max_sz, result);
   return 10;
 }
