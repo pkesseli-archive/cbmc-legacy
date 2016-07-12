c289522e51ff837d74d902fd8517a3b1773db4f0
Pascal Kesseli <pascal_kesseli@hotmail.com>
2016-07-12 10:24:59 +0100

    Added ce output stub.

--- src/cegis/facade/cegis.h
+++ src/cegis/facade/cegis.h
@@ -38,7 +38,6 @@ int run_cegis(learnt &learn, oraclet &oracle, preproct &preproc, seedt &seed, si
   learn.seed(seed);
   do
   {
-    learn.show_candidate(os); // XXX: Debug
     const typename learnt::candidatet &candidate=learn.next_candidate();
     oracle.verify(candidate);
   } while (oracle.has_counterexamples()
